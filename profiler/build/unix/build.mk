b39322dd9f67e2b4aa0c530957ce7ff9e62b7dd6
Paul Alesius <paul@unnservice.com>
2022-12-17 05:28:50 +0100

    Builds and works for me
    Signed-off-by: Paul Alesius <paul@unnservice.com>

--- profiler/build/unix/build.mk
+++ profiler/build/unix/build.mk
@@ -1,14 +1,17 @@
-CFLAGS +=
+CFLAGS += -Wstringop-overflow=0
 CXXFLAGS := $(CFLAGS) -std=c++17
 DEFINES += -DIMGUI_ENABLE_FREETYPE
-INCLUDES := $(shell pkg-config --cflags glfw3 freetype2 capstone) -I../../../imgui
-LIBS := $(shell pkg-config --libs glfw3 freetype2 capstone) -lpthread -ldl
+INCLUDES := $(shell pkg-config --cflags freetype2 capstone wayland-egl egl wayland-cursor) -I../../../imgui
+LIBS := $(shell pkg-config --libs freetype2 capstone wayland-egl egl wayland-cursor glesv2) -lpthread -ldl
 
 PROJECT := Tracy
 IMAGE := $(PROJECT)-$(BUILD)
 
-FILTER := ../../../nfd/nfd_win.cpp
+FILTER := ../../../nfd/nfd_win.cpp ../../src/BackendGlfw.cpp ../../src/imgui/imgui_impl_glfw.cpp
 include ../../../common/src-from-vcxproj.mk
+
+SRC += ../../src/BackendWayland.cpp
+SRC2 += ../../src/wayland/xdg-shell.c ../../src/wayland/xdg-activation.c ../../src/wayland/xdg-decoration.c
 
 ifdef TRACY_NO_FILESELECTOR
 	CXXFLAGS += -DTRACY_NO_FILESELECTOR
